b75c734df6cd3988acb42366b261ca2002e3076a
Daniel Gysi <danielgysi@protonmail.com>
2023-10-08 15:08:56 -0400

    First pass at new assemble logic with minimal code to cover jp instructions, probably has errors

---
+++ src/assemble.rs
@@ -0,0 +1,49 @@
+use parse::{self, AsmArgument};
+
+/// An error that occured while parsing the assembly string
+#[derive(Debug)]
+pub enum AssembleError{
+    UnknownOp,
+    MissingArgs,
+    ExtraArgs,
+    InvalidArg,
+}
+
+/// For a line of assembly, emit its machine code
+pub fn assemble_instruction(inst: &str) -> Result<u16, AssembleError>{
+    let tokens = inst.split_whitespace().collect::<Vec<&str>>();
+
+    match *tokens.get(0).expect("Attempt to parse empty string as instruction") {
+        "CLS" => Ok(0x00E0),
+        "RET" => Ok(0x00EE),
+
+        "JP" => assemble_jp(tokens),
+
+
+        _ => Err(AssembleError::UnknownOp),
+    }
+}
+
+/// Given the tokens of a jp instrutction, return its machine code or an error
+fn assemble_jp(tokens: &[&str]) -> Result<u16, AssembleError>{
+    let args = parse::parse_asm_args(tokens[1..]);
+    match args.len() {
+        1 => {
+            let addr = parse::parse_valid_addr(args[0])?;
+            Ok(0x1000 + addr)
+        }
+
+        2 => {
+            match args[0] {
+                AsmArgument::Register(0u8) =>  {
+                    let addr = parse::parse_valid_adddr(args[1])?;
+                    Ok(0xB000 + addr)
+                }
+                _ => Err(AssemlbeError::InvalidArg)
+            }
+        }
+
+        0 => Err(AssembleError::MissingArgs),
+        _ => Err(AssemblError::ExtraArgs)
+    }
+}
